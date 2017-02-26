bb60661100c2d48bf0489e30c414577492bb21f3
Louis-Saglio <louis.saglio@ynov.com>
2017-02-26 19:20:28 +0100

    Encapsulation en fonction. Ajout de la fonction create_object servant à créer automatiquement des listes d'instances. Ajout du test unitaire.

--- getter_setter_maker.py
+++ getter_setter_maker.py
@@ -1,22 +1,55 @@
-nom_classe = input("Entrez le nom de la classe: ")
-input_attributs = input("Entrez le nom des attributs séparés par des virgules:  ") + ','
-tab = "    "
+def create_class(verbose=False, nom_classe=None, input_attributs=None, path="Auto", tab='    '):
+    if verbose:
+        nom_classe = input("Entrez le nom de la classe: ")
+        input_attributs = input("Entrez le nom des attributs séparés par des virgules:  ")
+        tab = '    '
 
-attributs = []
-attr = ''
-for lettre in input_attributs:
-    if lettre != ',':
-        attr += lettre
-    else:
-        attributs.append(attr)
-        attr = ''
+    input_attributs += ','
+    attributs = []
+    attr = ''
+    for lettre in input_attributs:
+        if lettre != ',':
+            attr += lettre
+        else:
+            attributs.append(attr)
+            attr = ''
 
-print("class ", nom_classe, ':\n', sep='', file=open(nom_classe.lower() + '.py', 'w'))
-print(tab, "def __init__(self):", sep='', file=open(nom_classe.lower() + '.py', 'a'))
-for attribut in attributs:
-    print(tab, tab, "self.", attribut, " = \"Undefined\"", sep='', file=open(nom_classe.lower() + '.py', 'a'))
-print("\n", end='', file=open(nom_classe.lower() + '.py', 'a'))
-for attribut in attributs:
-    print(tab, "# Gestion de ", attribut, sep='', file=open(nom_classe.lower() + '.py', 'a'))
-    print(tab, "def get_", attribut, "(self):\n", tab, tab, "return self.", attribut, sep='', end='\n\n', file=open(nom_classe.lower() + '.py', 'a'))
-    print(tab, "def set_", attribut, "(self, new_", attribut, "):\n", tab, tab, "self.", attribut, " = new_", attribut, sep='', end='\n\n', file=open(nom_classe.lower() + '.py', 'a'))
+    if path == "Auto":
+        fichier = "classes/" + nom_classe.lower() + '.py'
+
+    with open(fichier, 'a') as dest:
+        print("class ", nom_classe, ':\n', sep='', file=dest)
+        print(tab, "def __init__(self, attributs):", sep='', file=dest)
+        print(tab, tab, "self.attr = attributs", sep='', file=dest)
+        for attribut in attributs:
+            print(tab, tab, "self.set_", attribut, "(attributs[\"", attribut, "\"])", sep='', file=dest)
+        print("\n", end='', file=dest)
+        for attribut in attributs:
+            print(tab, "# Gestion de ", attribut, sep='', file=dest)
+            print(tab, "def get_", attribut, "(self):\n", tab, tab, "return self.", attribut, sep='', end='\n\n', file=dest)
+            print(tab, "def set_", attribut, "(self, new_", attribut, "):\n", tab, tab, "self.", attribut, " = new_", attribut, sep='', end='\n\n', file=dest)
+
+
+def create_object(classe, attributs):
+    from copy import deepcopy
+    objets = []
+    noms_attr = []
+    dico = {}
+    for key in attributs:
+        noms_attr.append(key)
+        dico[key] = []
+    for i in range(len(attributs[noms_attr[0]])):
+        for n in noms_attr:
+            dico[n] = attributs[n][i]
+        objets.append(classe(dico))
+        objets = deepcopy(objets)
+    return objets
+
+
+if __name__ == "__main__":
+    create_class(nom_classe="Pion", input_attributs="nom,position")
+    from classes.pion import Pion
+    from os import remove
+    objets = create_object(Pion, {"nom": ["pion1", "pion2"], "position": [1, 2]})
+    print(objets[1].get_nom(), objets[0].get_position())
+    remove("classes/pion.py")
