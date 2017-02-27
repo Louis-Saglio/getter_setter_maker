085d9fb4f15fd5bd03bda68ee4c95e0a77a61746
Louis-Saglio <louis.saglio@sfr.fr>
2017-02-27 15:21:12 +0100

    La fonction create_class_instance est mainteneant effective grâce à la fonction exec()

--- getter_setter_maker.py
+++ getter_setter_maker.py
@@ -2,16 +2,16 @@ def create_class(verbose=False, nom_classe=None, input_attributs=None, tab='    
     if verbose:
         nom_classe = input("Entrez le nom de la classe: ")
         input_attributs = input("Entrez le nom des attributs séparés par des virgules:  ")
-
-    input_attributs += ','
-    attributs = []
-    attr = ''
-    for lettre in input_attributs:
-        if lettre != ',':
-            attr += lettre
-        else:
-            attributs.append(attr)
-            attr = ''
+        input_attributs += ','
+        attributs = []
+        attr = ''
+        for lettre in input_attributs:
+            if lettre != ',':
+                attr += lettre
+            else:
+                attributs.append(attr)
+                attr = ''
+    attributs = input_attributs
 
     with open("classes.py", 'a') as dest:
         print("class ", nom_classe, ':\n', sep='', file=dest)
@@ -44,13 +44,27 @@ def create_object(classe, attributs):
 
 
 def create_class_instance(nom_classe, attributs):
-        pass
-
+    # On récupère le nom des attributs sous forme de liste pour la fonction create_class
+    liste_attributs = []
+    for key in attributs:
+        liste_attributs.append(key)
+    try:
+        exec("from classes import " + nom_classe)
+    except:
+        # liste
+        create_class(nom_classe=nom_classe, input_attributs=liste_attributs)
+        exec("from classes import " + nom_classe)
+    # dictionnaire
+    exec("objets = create_object(" + nom_classe + ", attributs)")
+    return objets
 
 if __name__ == "__main__":
-    create_class(nom_classe="Pion", input_attributs="nom,position")
-    from classes import Pion
-    objets = create_object(Pion, {"nom": ["pion1", "pion2"], "position": [1, 2]})
-    print(objets[1].get_nom(), objets[0].get_position())
-    # from os import remove
-    # remove("classes/pion.py")
+    # create_class(nom_classe="Pion", input_attributs="nom,position")
+    # from classes import Pion
+    # objets = create_object(Pion, {"nom": ["pion1", "pion2"], "position": [1, 2]})
+    # print(objets[1].get_nom(), objets[0].get_position())
+    # # from os import remove
+    # # remove("classes/pion.py")
+    create_class_instance("Test", {"nom": ["pion1", "pion2"], "position": [1, 2]})
+    # create_class(True)
+    # create_class(nom_classe="Test", input_attributs=["nom", "prenom"])
