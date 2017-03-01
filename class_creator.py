1e888ab761b57fd3fd68a2ca39efcd3a995d759b
Louis-Saglio <louis.saglio@sfr.fr>
2017-03-01 20:22:35 +0100

    Mise à jour pour être compatible avec la fonction add_attribute_to_class Nettoyage : tab, tab => tab * 2,

--- class_creator.py
+++ class_creator.py
@@ -19,14 +19,17 @@ with open("classes/" + nom_classe.lower() + ".py", 'a') as dest:
 
     # Ecriture de __init__
     print(tab, "def __init__(self, attributs):", sep='', file=dest)
-    print(tab, tab, "self.attr = attributs", sep='', file=dest)
+    print(tab * 2, "self.attr = attributs", sep='', file=dest)
     for attribut in attributs:
-        print(tab, tab, "self.set_", attribut, "(attributs[\"", attribut, "\"])", sep='', file=dest)
+        print(tab * 2, "self.set_", attribut, "(attributs[\"", attribut, "\"])", sep='', file=dest)
+    print(tab * 2, "# Attention ! Toujours laisser la ligne suivante a la fin de la methode init\n", tab * 2,
+          "# Fin de la methode init",
+          sep='', file=dest)
     print("\n", end='', file=dest)
 
     # Ecriture des getters et setters
     for attribut in attributs:
         print(tab, "# Gestion de ", attribut, sep='', file=dest)
-        print(tab, "def get_", attribut, "(self):\n", tab, tab, "return self.", attribut, sep='', end='\n\n', file=dest)
-        print(tab, "def set_", attribut, "(self, new_", attribut, "):\n", tab, tab, "self.", attribut, " = new_",
+        print(tab, "def get_", attribut, "(self):\n", tab * 2, "return self.", attribut, sep='', end='\n\n', file=dest)
+        print(tab, "def set_", attribut, "(self, new_", attribut, "):\n", tab * 2, "self.", attribut, " = new_",
               attribut, sep='', end='\n\n', file=dest)
